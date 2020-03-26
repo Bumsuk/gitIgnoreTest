6759b311b71316f288822414fabd01ce52055be1
bumsuk <xenix72@gmail.com>
2020-03-26 14:56:11 +0900

    add xcode project with pods

---
+++ gitIgnore/gitIgnore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitIgnore
+//
+//  Created by brown on 2020/03/26.
+//  Copyright © 2020 brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
