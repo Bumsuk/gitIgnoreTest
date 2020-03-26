48daca7f3a35b34caf47b689cdc09264083d10fd
bumsuk <xenix72@gmail.com>
2020-03-26 15:01:13 +0900

    chage settings

--- gitIgnore/gitIgnore/ViewController.swift
+++ gitIgnore/gitIgnore/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        _ = Observable<Int>.timer(.seconds(4), scheduler: MainScheduler.instance)
+        .subscribe(onNext: { _ in
+            print("🤡 점심먹자!")
+        })
+        
     }
 
 
